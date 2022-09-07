f00efc6739c08205d4eacb3926791d2b1e89488e
Damish <damishs88@gmail.com>
2022-09-07 12:51:05 +0530

    dynamic routes pages /id

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,28 +1,29 @@
+import Link from 'next/link';
 import styles from '../../styles/Ninjas.module.css'
 
-export const getStaticProps = async ()=>{
+export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
-    
-    return{
-        props: {ninjas:data}
+
+    return {
+        props: { ninjas: data }
     }
 }
 
-const Ninjas = ({ninjas}) => {
-    
-    return ( 
+const Ninjas = ({ ninjas }) => {
+
+    return (
         <div>
             <h1>All Ninjas</h1>
-            {ninjas.map(ninja =>(
-                <div key = {ninja.id}>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
                     <a className={styles.single}>
                         <h3>{ninja.name}</h3>
                     </a>
-                </div>
+                </Link>
             ))}
         </div>
-     );
+    );
 }
- 
+
 export default Ninjas;
